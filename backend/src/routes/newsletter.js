0b12c01de46ad4ebd3ff1a50bb9a0fde6c21bc05
Harishkumar39 <harish7299152036@gmail.com>
2026-03-22 13:40:24 +0100

    Fix: newsletter

--- backend/src/routes/newsletter.js
+++ backend/src/routes/newsletter.js
@@ -7,7 +7,7 @@ dotenv.config()
 const router = express.Router();
 
 
-router.post('/subscribe', async (req, res) => {
+router.post('/', async (req, res) => {
   const { email } = req.body;
 
   if (!email) return res.status(400).json({ message: "Email is required" });
